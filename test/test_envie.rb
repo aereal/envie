44a2a39188f1e05f645fe64e660e147a6c02f0af
aereal <aereal@aereal.org>
2014-12-06 15:09:32 +0900

    Use spec style

--- test/test_envie.rb
+++ test/test_envie.rb
@@ -1,11 +1,7 @@
 require 'minitest_helper'
 
-class TestEnvie < MiniTest::Unit::TestCase
-  def test_that_it_has_a_version_number
-    refute_nil ::Envie::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+describe Envie do
+  it "has a version" do
+    refute_nil Envie::VERSION
   end
 end
